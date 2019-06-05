f5ff5c43f1ebb56a3ff89ddfbaaaca5cd6c0e8a7
Neal Caren <neal.caren@gmail.com>
2019-06-05 12:24:09 -0400

    Rename columns

--- pdtext/topic_models.py
+++ pdtext/topic_models.py
@@ -13,12 +13,17 @@ def topic_words(lda_model, vectorizer ):
     word_topic_scores = lda_model.components_.T
     vocabulary        = vectorizer.get_feature_names()
 
+    number_of_topics = lda.get_params()['n_components']
+    column_names = ['Topic %s' % i for i in range(1, number_of_topics + 1)]
 
     topic_words_df = pd.DataFrame(word_topic_scores,
-                                  index = vocabulary)
+                                  index = vocabulary,
+                                  columns = column_names)
 
     topic_words_df = topic_words_df.apply(helpers.column_swap).reset_index(drop = True).rename_axis('rank')
 
     topic_words_df.index = topic_words_df.index + 1
+
+    topic_words_df = topic_words_df.add_prefix('Topic ')
 
     return topic_words_df
