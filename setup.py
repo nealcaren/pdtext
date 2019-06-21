11e854acc1919f0b4db843765662c9944fde3c3e
Neal Caren <neal.caren@gmail.com>
2019-06-21 16:34:11 -0400

    First spacy tool

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ setup(
     name="pdtext",
     version="0.1.2",
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib',
-                      'scikit-learn', 'pandas', 'requests'
-                     ],
+    install_requires=["numpy", "matplotlib", "scikit-learn", "pandas", "requests"],
     include_package_data=True,
-
     author="Neal Caren",
     author_email="neal.caren@gmail.com",
     description=("Helper functions for working with text in pandas"),
