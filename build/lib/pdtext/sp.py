2c19862954e494ba0ad14b5732d6d5fa9025274a
Neal Caren <neal.caren@gmail.com>
2019-06-21 16:35:28 -0400

    Typo

--- build/lib/pdtext/sp.py
+++ build/lib/pdtext/sp.py
@@ -1,6 +1,6 @@
 def extract_pos(doc, pos, return_list = True):
     tokens = [t.text for t in doc if t.pos_ == pos]
-    if return_list = False:
+    if return_list == False:
         return ', '.join(tokens)
 
     return tokens
