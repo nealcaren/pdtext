c9b2fcf3f9a35781c897904a0c7be37882037752
Neal Caren <neal.caren@gmail.com>
2020-06-02 14:06:35 -0400

    Rebuild

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdtext",
-    version="0.1.2",
+    version="0.3.0",
     packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "scikit-learn", "pandas", "requests"],
+    install_requires=[],
     include_package_data=True,
     author="Neal Caren",
     author_email="neal.caren@gmail.com",
     description=("Helper functions for working with text in pandas"),
     license="BSD",
-    keywords="pandas ",
+    keywords="pandas",
     url="https://github.com/nealcaren/pdtext",
 )
